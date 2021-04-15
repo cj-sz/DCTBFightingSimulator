a586fb28e21a74c4a90eeb7dc29e57275aab1740
Caleb Solomon <cj5bossprofessional@gmail.com>
2021-04-15 06:35:49 -0400

    catchup

--- DCTBFightingSimulator/Form1.cs
+++ DCTBFightingSimulator/Form1.cs
@@ -85,19 +85,18 @@ namespace DCTBFightingSimulator
         {
             //Current version: 0.0.1
             changelogText.Text = "";
-            changelogText.AppendText("VERSON: 0.1.0");
-            changelogText.AppendText(Environment.NewLine + "Initial release (#1) of DCTBFightingSimulator!" + Environment.NewLine);
-            changelogText.AppendText(Environment.NewLine + "- Current features:");
-            changelogText.AppendText(Environment.NewLine + "- 30 Characters (2 of each type)");
-            changelogText.AppendText(Environment.NewLine + "- Computer vs. Computer Simulations");
-            changelogText.AppendText(Environment.NewLine + "- Randomized AI");
-            changelogText.AppendText(Environment.NewLine + "- Live stat changes and text window of events");
-            changelogText.AppendText(Environment.NewLine + "- Character Database");
-            changelogText.AppendText(Environment.NewLine + "- Character Creation feature (please submit builds to the github (use the 'View Page' link to go directly to the github; also see the wiki))");
+            changelogText.AppendText("VERSON: 0.1.0"); //NEEDS CHANGING
+            changelogText.AppendText(Environment.NewLine + "Initial release (#1) of DCTBFightingSimulator!" + Environment.NewLine); //NEEDS CHANGING
+            changelogText.AppendText(Environment.NewLine + Environment.NewLine + "v0.1.0 Changelog:"); //UPDATE NUMBER
+            changelogText.AppendText(Environment.NewLine + "ADDITIONS:");
+            changelogText.AppendText(Environment.NewLine + "- Added DCTBFSSeeding document, with tournament seeding for all existing characters");
+            changelogText.AppendText(Environment.NewLine + "CHANGES:");
+            changelogText.AppendText(Environment.NewLine + "BALANCING:");
+            //Post-Changes
             changelogText.AppendText(Environment.NewLine + Environment.NewLine + "This is only the initial release with baseline simulations working; expect PvE and local PvP features to come soon (see the roadmap for details) Please bring up any issues, questions, user-submitted builds, etc. on the github page.");
             changelogText.AppendText(Environment.NewLine + "Additionally, every time there is an update involving additional characters, new AI, or new gameplay features, an official tournament will be run. The link to view tournament results of the simulation between all characters will be found here. Custom-created, user-submitted builds will also be a part of the competition.");
             changelogText.AppendText(Environment.NewLine + Environment.NewLine + "View the most recent tournament(which may eventually be moved onto this program itself): https://challonge.com/bz19xyip");
-            changelogText.AppendText(Environment.NewLine + "Tournament Archives:");
+            changelogText.AppendText(Environment.NewLine + "Tournament Archives:"); //NEEDS CHANGING
             changelogText.AppendText(Environment.NewLine + "No past tournaments to view.");
         }
         public void fillRoadmap()
