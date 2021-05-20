195370a656461f65ee906d3812af87e3eae3318b
Caleb Solomon <cj5bossprofessional@gmail.com>
2021-05-20 12:30:07 -0400

    changelog, roadmap, opening ui

--- DCTBFightingSimulator/Form1.Designer.cs
+++ DCTBFightingSimulator/Form1.Designer.cs
@@ -683,7 +683,7 @@ namespace DCTBFightingSimulator
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(119, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Current Release: v0.2.1";
+            this.label1.Text = "Current Release: v0.2.2";
             // 
             // button2
             // 
@@ -754,7 +754,7 @@ namespace DCTBFightingSimulator
             this.welcomeLabel1.Name = "welcomeLabel1";
             this.welcomeLabel1.Size = new System.Drawing.Size(856, 51);
             this.welcomeLabel1.TabIndex = 10;
-            this.welcomeLabel1.Text = "Welcome to DCTBFightingSimulator v0.2.1!";
+            this.welcomeLabel1.Text = "Welcome to DCTBFightingSimulator!";
             // 
             // welcomeLabel2
             // 
@@ -7003,8 +7003,8 @@ namespace DCTBFightingSimulator
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1432, 853);
-            this.Controls.Add(this.startingPanel);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.startingPanel);
             this.Controls.Add(this.characterDatabasePanel);
             this.Controls.Add(this.EvEPanel);
             this.Controls.Add(this.pictureBox2);
@@ -7019,7 +7019,7 @@ namespace DCTBFightingSimulator
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "DCTBFightingSimulator";
-            this.Text = "DCTBFightingSimulator v0.2.0";
+            this.Text = "DCTBFightingSimulator v0.2.2";
             ((System.ComponentModel.ISupportInitialize)(this.CCN1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.CCN2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.CCN3)).EndInit();
