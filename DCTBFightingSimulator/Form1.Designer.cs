7557645ba0c733a1920c8c0d77062e188987a691
Caleb Solomon <cj5bossprofessional@gmail.com>
2021-05-22 21:22:19 -0400

    final v0.2.3 build

--- DCTBFightingSimulator/Form1.Designer.cs
+++ DCTBFightingSimulator/Form1.Designer.cs
@@ -686,7 +686,7 @@ namespace DCTBFightingSimulator
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(119, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Current Release: v0.2.2";
+            this.label1.Text = "Current Release: v0.2.3";
             // 
             // button2
             // 
@@ -7055,7 +7055,7 @@ namespace DCTBFightingSimulator
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "DCTBFightingSimulator";
-            this.Text = "DCTBFightingSimulator v0.2.2";
+            this.Text = "DCTBFightingSimulator v0.2.3";
             ((System.ComponentModel.ISupportInitialize)(this.CCN1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.CCN2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.CCN3)).EndInit();
